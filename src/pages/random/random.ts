998fa1f8cdfc3f57b241982bd953d0e7f0716947
cfoster5 <cfoster3204@gmail.com>
2018-01-20 23:54:02 -0600

    Started on dark mode and random page fires randomize function when constructor loads
    Dark mode only works for restaurants, not searchbar, tabs, or nav.

--- src/pages/random/random.ts
+++ src/pages/random/random.ts
@@ -10,57 +10,58 @@ export class RandomPage {
   restaurant: string;
 
   constructor(public navCtrl: NavController) {
-
-    var restaurants = [
-        'Applebee’s',
-        'Azzip',
-        'Bandana’s',
-        'Bar Louie',
-        'Biaggi’s',
-        'Big Bang',
-        'Bob Evans',
-        'Bonefish',
-        'Buffalo Wild Wings',
-        'Chick-fil-a',
-        'Chili’s',
-        'Comfort',
-        'Cracker Barrel',
-        'Culver’s',
-        'Five Guys',
-        'Fujiyama',
-        'Gersthaus',
-        'Hacienda',
-        'IHOP',
-        'Logan’s',
-        'Longhorn',
-        'Los Bravos',
-        'McAlister’s',
-        'Mission BBQ',
-        'Mod Pizza',
-        'Moe’s',
-        'O’Charley’s',
-        'Old Chicago',
-        'Olive Garden',
-        'Outback Steakhouse',
-        'Panera',
-        'Pie Pan',
-        'Rafferty’s',
-        'Red Lobster',
-        'Red Robin',
-        'Steak n Shake',
-        'Texas Roadhouse',
-        'TGI Friday’s',
-        'Turoni’s',
-        'Western Ribeye',
-        'Zuki'
-      ];
-
-    // var restaurant = restaurants[Math.floor(Math.random()*restaurants.length)]; //Was not working
-    this.restaurant = restaurants[Math.floor(Math.random()*restaurants.length)];
-    console.log(this.restaurant);
+    this.randomize(); //FIRES RANDOMIZE() WHEN CONSTRUCTOR IS FIRED
+    // var restaurants = [
+    //     'Applebee’s',
+    //     'Azzip',
+    //     'Bandana’s',
+    //     'Bar Louie',
+    //     'Biaggi’s',
+    //     'Big Bang',
+    //     'Bob Evans',
+    //     'Bonefish',
+    //     'Buffalo Wild Wings',
+    //     'Chick-fil-a',
+    //     'Chili’s',
+    //     'Comfort',
+    //     'Cracker Barrel',
+    //     'Culver’s',
+    //     'Five Guys',
+    //     'Fujiyama',
+    //     'Gersthaus',
+    //     'Hacienda',
+    //     'IHOP',
+    //     'Logan’s',
+    //     'Longhorn',
+    //     'Los Bravos',
+    //     'McAlister’s',
+    //     'Mission BBQ',
+    //     'Mod Pizza',
+    //     'Moe’s',
+    //     'O’Charley’s',
+    //     'Old Chicago',
+    //     'Olive Garden',
+    //     'Outback Steakhouse',
+    //     'Panera',
+    //     'Pie Pan',
+    //     'Rafferty’s',
+    //     'Red Lobster',
+    //     'Red Robin',
+    //     'Steak n Shake',
+    //     'Texas Roadhouse',
+    //     'TGI Friday’s',
+    //     'Turoni’s',
+    //     'Western Ribeye',
+    //     'Zuki'
+    //   ];
+    //
+    // // var restaurant = restaurants[Math.floor(Math.random()*restaurants.length)]; //Was not working
+    // this.restaurant = restaurants[Math.floor(Math.random()*restaurants.length)];
+    // console.log(this.restaurant);
   }
 
   randomize(){
+    // this.constructor() //FIRE CONSTRUCTOR WHEN RANDOMIZE() IS FIRED
     var restaurants = [
         'Applebee’s',
         'Azzip',
