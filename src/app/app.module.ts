8d662c0e901803ac36d1fefd7ba893e702f41209
cfoster5 <cfoster3204@gmail.com>
2018-06-03 20:14:46 -0500

    Added provider/service for Firebase data and updated app.module.ts for FB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database-deprecated';
+// import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDfQuQQnosmf47h8GGJ_Cns03bMyb0-VM",
+  authDomain: "restaurantapp-68e2f.firebaseapp.com",
+  databaseURL: "https://restaurantapp-68e2f.firebaseio.com",
+  projectId: "restaurantapp-68e2f",
+  storageBucket: "restaurantapp-68e2f.appspot.com",
+  messagingSenderId: "635105165174"
+};
 
 import { RandomPage } from '../pages/random/random';
 import { ContactPage } from '../pages/contact/contact';
@@ -12,6 +24,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FirebaseProvider } from '../providers/firebase/firebase';
 
 @NgModule({
   declarations: [
@@ -25,7 +38,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     FormsModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    // AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -39,7 +55,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AngularFireDatabase,
+    FirebaseProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/providers/firebase/firebase.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class FirebaseProvider {
+
+  constructor(public db: AngularFireDatabase,) {
+    console.log('Hello FirebaseProvider Provider');
+  }
+
+  getList(): FirebaseListObservable<any> {
+    return this.db.list('list', {
+      query: {
+        // orderByChild:'order',
+      }
+    });
+  }
+
+  addRestaurant(restaurantName){
+    firebase.database().ref('list').push(restaurantName);
+  }
+
+}
